533ca747221084e16742599d4bab923bb56a5204
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-04-08 10:52:31 -0400

    Bold heading

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -2,7 +2,7 @@ export function PageHeading({ children }: { children: string }) {
   return (
     <div className="">
       <h1 className="text-gray-500 dark:text-gray-400 flex-shrink-0">
-        <span className="block">iMessage2Spotify</span>
+        <span className="block font-bold">iMessage2Spotify</span>
         <span className="block uppercase text-2xl">{children}</span>
       </h1>
     </div>
