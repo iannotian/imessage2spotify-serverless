2cc6f60ae1a85785badb9d19ece6cf6187a52c03
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-08-21 10:54:29 -0700

    Revise heading design

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -2,8 +2,8 @@ export function PageHeading({ children }: { children: string }) {
   return (
     <div className="">
       <h1 className="text-gray-500 dark:text-gray-400 flex-shrink-0">
-        <span className="block font-bold">iMessage2Spotify</span>
-        <span className="block uppercase text-2xl">{children}</span>
+        <span className="block">iMessage2Spotify</span>
+        <span className="block text-4xl">{children}</span>
       </h1>
     </div>
   );
