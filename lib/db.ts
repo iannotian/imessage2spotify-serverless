2de95a2a81a21046b0b65c8d8b799149840b77ef
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-08-20 18:01:52 -0700

    Add DB helper functions

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,45 @@
 import { PrismaClient } from "@prisma/client";
+import { SpotifyTrack } from "./types";
 
 const prisma = new PrismaClient();
 
 export const findAllTracks = async () => {
   return await prisma.track.findMany();
+};
+
+export const findTrackBySpotifyId = async (spotifyTrackId: string) => {
+  return await prisma.track.findUnique({
+    where: {
+      spotifyTrackId,
+    },
+  });
+};
+
+export const saveTrack = async (track: SpotifyTrack) => {
+  const { id } = track;
+
+  const existingTrack = await findTrackBySpotifyId(id);
+
+  if (existingTrack) {
+    return await prisma.track.update({
+      where: {
+        id: existingTrack.id,
+      },
+      data: {
+        occurrences: existingTrack.occurrences + 1,
+      },
+    });
+  } else {
+    return await prisma.track.create({
+      data: {
+        title: track.name,
+        artist: track.artists.map((artist) => artist.name).join(", "),
+        album: track.album.name,
+        imageUrl: track.album.images[0].url,
+        spotifyTrackId: track.id,
+        spotifyUrl: track.uri,
+        spotifyPreviewUrl: track.preview_url,
+      },
+    });
+  }
 };
