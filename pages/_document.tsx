5f09de038658ddc649602f03deb1236bd65b946d
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2023-01-17 00:30:31 -0800

    Remove opacity from bg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="dark:bg-spotifyblack-800 text-gray-700 dark:text-gray-100 bg-gray-100/75 font-light m-4 lg:mx-auto max-w-4xl">
+      <body className="dark:bg-spotifyblack-800 text-gray-700 dark:text-gray-100 bg-gray-100 font-light m-4 lg:mx-auto max-w-4xl">
         <Main />
         <NextScript />
       </body>
